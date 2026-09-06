4c27051d2f4b640259a894d4f79e842cc3d67774
gopalkrishna <gopalakrishnadasari89@gmail.com>
2026-09-06 23:25:33 +0530

    chore: configure next.config.js and .eslintrc.json for production builds

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'github.com'],
+    domains: ['avatars.githubusercontent.com', 'github.com', 'images.unsplash.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
 module.exports = nextConfig;
+
